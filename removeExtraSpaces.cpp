c27e0ae30f82ddcca4f980e902d08755070240e5
wumucun <yixia1999@gmail.com>
2022-01-14 19:29:59 +0800

    去除空格

--- removeExtraSpaces.cpp
+++ removeExtraSpaces.cpp
@@ -3,17 +3,34 @@
 
 using namespace std;
 
-void removeExtraSpaces(strict& s) {
+void removeExtraSpaces(string& s);
+int main() {
+    string str = "  Bob loves    Alice ";
+    for (const auto& ch : str)
+        cout << ch;
+    cout << endl;
+    removeExtraSpaces(str);
+    for (const auto& ch : str)
+        cout << ch;
+    cout << endl;
+}
+
+void removeExtraSpaces(string& s) {
     int len = s.size();
     int slow_index = 0, fast_index = 0;
     //去除句前空格
-    while (len > 0 && s[fast_index] != ' ')
+    while (len > 0 && s[fast_index] == ' ')
         ++fast_index;
     //去除句中空格
     for (; fast_index < len; ++fast_index) {
-        if (fast_index > 1 && s[])
+        if (fast_index > 1 && s[fast_index] == s[fast_index - 1] && s[fast_index] == ' ')
+            continue;
+        else
+            s[slow_index++] = s[fast_index]; 
     }
-}
-int main() {
 
+    if (s[slow_index - 1] == ' ')
+        s.resize(slow_index - 1);
+    else       
+        s.resize(slow_index);
 }
