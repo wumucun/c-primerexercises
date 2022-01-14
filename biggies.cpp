8d5bbafbe9c5b23588151fc209aa1f1bd301306e
“wumucun” <yixia1999@gmail.com>
2022-01-14 14:02:01 +0800

    增加了lambda表达式应用，对于只接受单参数的谓词的算法，可以通过lambda捕获多的参数

--- biggies.cpp
+++ biggies.cpp
@@ -15,11 +15,34 @@ void elimDups(vector<string> & words) {
     words.erase(end_unique, words.end());
 }
 
+void biggies(vector<string> words, vector<string>::size_type sz) {
+    elimDups(words);
+    stable_sort(words.begin(), words.end(), 
+                [](const string & a, const string& b)
+                { return a.size() < b.size();});
+    auto wc = find_if(words.begin(), words.end(), [sz](const string& a)
+                                                  {return a.size() >= sz;});
+    for (auto it = wc; it != words.end(); ++it)
+        cout << *it << " ";
+    cout << endl;
+}
+
+
+
 int main() {
     vector<string> words = {"the", "quick", "red", "fox", "jumps", "over", "tne", "slow", "red", "turtle"};
-    elimDups(words);
-    for (auto& str : words)
-        cout << str << " ";
-    cout << endl;
+    // elimDups(words);
+    // cout << "字典序排序去重" << endl;
+    // for (auto& str : words)
+    //     cout << str << " ";
+    // cout << endl;
+    // cout << "按照长度排序" << endl;
+    // stable_sort(words.begin(), words.end(), 
+    //             [](const string & a, const string& b)
+    //             { return a.size() < b.size();});
+    // for (auto &str : words)
+    //     cout << str << " ";
+    // cout << endl;
+    biggies(words, 5);
 }
 
